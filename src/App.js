9ef8b34c178b035110c771b8b8f13fc43a830f60
Mario Zamora <mariozamora5126@gmail.com>
2025-02-18 08:54:18 -0600

    Comunicación entre los componentes, Props y atributos

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import './App.css';
-import { CreateTodoButton } from './CreateTodoButton';
-import { TodoCounter } from './TodoCounter';
-import { TodoItem } from './TodoItem';
-import { TodoList } from './TodoList';
-import { TodoSearch } from './TodoSearch';
+import "./App.css";
+import { CreateTodoButton } from "./CreateTodoButton";
+import { TodoCounter } from "./TodoCounter";
+import { TodoItem } from "./TodoItem";
+import { TodoList } from "./TodoList";
+import { TodoSearch } from "./TodoSearch";
+
+const defaultTodos = [
+  { text: "Cortar cebolla", completed: true },
+  { text: "Tomar el curso de intro a React", completed: false },
+  { text: "Llorar con la llorona", completed: false },
+  { text: "LALALALALA", completed: false },
+];
 
 function App() {
   return (
-    <div className="App">
-      <TodoCounter />
+    <>
+      <TodoCounter completed={16} total={25} />
       <TodoSearch />
 
       <TodoList>
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
+        {defaultTodos.map((todo) => (
+          <TodoItem
+            key={todo.text}
+            text={todo.text}
+            completed={todo.completed}
+          />
+        ))}
       </TodoList>
 
       <CreateTodoButton />
-
-    </div>
+    </>
   );
 }
 
